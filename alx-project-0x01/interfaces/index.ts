7c2a9759a1f44736422304dd67d7c7c316674dc1
Adegoke Philip <that_african_guy@yahoo.com>
2025-08-22 02:16:16 +0100

    task 1 and 2

---
+++ alx-project-0x01/interfaces/index.ts
@@ -0,0 +1,37 @@
+export interface PostProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+  posts: any;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
